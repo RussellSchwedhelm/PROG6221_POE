cffc7c5fbb503d58d9464b42f3cbc8fa15f25ca8
ST10065470 <st10065470@vcconnect.edu.za>
2023-05-28 14:50:06 +0200

    Implemented Calorie Messages

--- PROG6221_POE/Program.cs
+++ PROG6221_POE/Program.cs
@@ -316,7 +316,7 @@ namespace PROG6221_POE
                     Console.Write("Please Enter The Amount Of Calories In \""
                                   + ingredientName + "\": ");
                     userInput = Console.ReadLine();
-                    ingredientCalories = (int) errorControl.CheckForPositiveNumber(userInput);
+                    ingredientCalories = (int)errorControl.CheckForPositiveNumber(userInput);
                 } while (ingredientCalories == -1);
 
                 Console.WriteLine();
@@ -453,7 +453,7 @@ namespace PROG6221_POE
                 } while (recipeScale == 0); // Continue the loop if the recipe scale is not set
 
                 PrintTitle();
-                Console.WriteLine("Recipe: " + recipeName+ "\n");
+                Console.WriteLine("Recipe: " + recipeName + "\n");
                 Console.WriteLine(recipeToDisplay.DisplayRecipe(recipeScale)); // Display the recipe with the selected scale
 
                 if (recipeToDisplay.TotalCalories(recipeScale) > 300)
@@ -506,27 +506,40 @@ namespace PROG6221_POE
         //----------------------------------------------------------------------------\\
         // The information on the traditional food groups was provided by ChatGPT, an AI language model developed by OpenAI.
         // For more detailed information, please consult reliable nutrition sources or consult a healthcare professional.
+
+        // The DisplayFoodGroupInfo method prints information about different food groups, ensuring line wrapping at 75 characters.
+        // It splits the information into words, printing each word and adding a newline when necessary to maintain the line length.
+        // After displaying the information, it prompts the user to press 'Enter' to return to the menu.
         public void DisplayFoodGroupInfo()
         {
+            // Define the information for each food group
             string fruits = "Fruits: Sweet and refreshing plant-based foods that provide essential vitamins, minerals, fiber, and antioxidants. Examples include apples, oranges, berries, and melons.";
             string vegetables = "Vegetables: Nourishing and diverse plant-based foods that offer a wide range of vitamins, minerals, fiber, and beneficial plant compounds. Examples include leafy greens, broccoli, carrots, and peppers.";
             string grains = "Grains: Staple foods made from cereal crops such as wheat, rice, oats, and corn. Grains are a significant source of carbohydrates, fiber, and various nutrients like B vitamins and minerals. Examples include bread, rice, pasta, and cereals.";
             string protein = "Protein: Foods that are rich in protein, necessary for building and repairing body tissues. This group includes animal sources like meat, poultry, fish, eggs, and dairy, as well as plant-based sources like legumes (beans, lentils), tofu, nuts, and seeds.";
             string dairy = "Dairy: Dairy products, such as milk, cheese, and yogurt, are excellent sources of calcium, protein, and other essential nutrients. Dairy provides bone-strengthening minerals and can be part of a healthy diet. Plant-based alternatives like soy or almond milk are also available for those who avoid dairy.";
             string fatsAndOils = "Fats or Oils: This group includes fats and oils that provide energy and essential fatty acids. While it's important to consume them in moderation, healthy fats from sources like avocados, nuts, seeds, and olive oil can be part of a balanced diet.";
+
+            // Create an array to store the food group information
             string[] foodGroups = { fruits, vegetables, grains, protein, dairy, fatsAndOils };
+
+            // Initialize variables
             string[] info;
             int limit = 0;
-            PrintTitle();
 
+            PrintTitle();
             Console.WriteLine("Here Is Information About The Food Groups Which Encompass All Ingredients:\n");
 
+            // Iterate over each food group
             for (int group = 0; group < 6; group++)
             {
+                // Split the information into words
                 info = foodGroups[group].Split(' ', '-');
 
+                // Iterate over each word in the food group information
                 foreach (string word in info)
                 {
+                    // Check if adding the word exceeds the character limit
                     if (limit + word.Length > 75)
                     {
                         Console.WriteLine();
@@ -536,12 +549,13 @@ namespace PROG6221_POE
                     Console.Write(word + ' ');
                     limit += word.Length + 1;
                 }
+
                 Console.WriteLine("\n");
             }
 
-            Console.Write("\n\nPress 'Enter' To Return To Menu...");
+            Console.Write("\nPress 'Enter' To Return To Menu...");
             Console.ReadLine();
         }
     }
-}
+    }
 //----------------------------------------------------------------------------\\
